eb82985eaf446b3d7b07dc7f7242786e2284f2f0
Vinhng1 <nguyenvinh130500@gmail.com>
2024-09-19 15:03:24 +0700

    boivaoday

---
+++ ifelse.js
@@ -0,0 +1,6 @@
+let a = prompt("What is the “official” name of JavaScript?",'') //tai sao lai phai co '' o trong tieu de
+if (a == "ECMAScript”") {
+    alert("Right");
+} else (a == "ECMAScript”")
+{ alert("Didn’t know? ECMAScript!")
+}
